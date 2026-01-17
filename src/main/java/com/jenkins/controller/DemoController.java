2d6867fd56ed3700b509040f077ba31a563a5ac7
akshaya5310 <akshayamahanty2014@gmail.com>
2026-01-17 15:50:32 +0530

    added controller

---
+++ src/main/java/com/jenkins/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+
+    @GetMapping(value = "/")
+    public String Hello(){
+        return "Hello User";
+    }
+
+}
